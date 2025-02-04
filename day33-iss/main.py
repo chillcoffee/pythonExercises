fa35751c7143b11130776037db203e8107294c14
Ruffa <ruffaresentes@me.com>
2025-02-04 14:59:12 +0800

    done func if dark and iss loc

--- day33-iss/main.py
+++ day33-iss/main.py
@@ -1,35 +1,48 @@
 import requests
 from datetime import datetime
+import smtplib
 
-MY_LAT = 51.507351 # Your latitude
-MY_LONG = -0.127758 # Your longitude
+MY_EMAIL = "chillcoffee.9419@gmail.com"
+MY_PASSWORD = "uwrqcnkcrhccqqje"
+MY_LAT = 11.694760 # Your latitude
+MY_LONG = 122.366859 # Your longitude
 
-response = requests.get(url="http://api.open-notify.org/iss-now.json")
-response.raise_for_status()
-data = response.json()
-
-iss_latitude = float(data["iss_position"]["latitude"])
-iss_longitude = float(data["iss_position"]["longitude"])
-
-#Your position is within +5 or -5 degrees of the ISS position.
+def is_iss_near():
+    response = requests.get(url="http://api.open-notify.org/iss-now.json")
+    response.raise_for_status()
 
 
-parameters = {
-    "lat": MY_LAT,
-    "lng": MY_LONG,
-    "formatted": 0,
-}
 
-response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
-response.raise_for_status()
-data = response.json()
-sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
-sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
+    data = response.json()
 
-time_now = datetime.now()
+    iss_latitude = float(data["iss_position"]["latitude"])
+    iss_longitude = float(data["iss_position"]["longitude"])
 
-#If the ISS is close to my current position
-# and it is currently dark
+    #My position is within +5 or -5 degrees of the ISS position.
+    if MY_LAT - 5 <= iss_latitude <= MY_LAT + 5 and MY_LONG - 5 <= iss_longitude <= MY_LONG + 5:
+        return True
+
+def its_dark():
+    parameters = {
+        "lat": MY_LAT,
+        "lng": MY_LONG,
+        "formatted": 0,
+    }
+
+    response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+    response.raise_for_status()
+    data = response.json()
+    sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+    sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
+
+    time_now = datetime.now().hour
+
+    if time_now >= sunset or time_now <= sunrise:
+        return True
+
+
+if is_iss_near() and its_dark():
+    print("Look up!")
 # Then send me an email to tell me to look up.
 # BONUS: run the code every 60 seconds.
 
