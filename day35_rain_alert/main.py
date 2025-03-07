044be92e21c0672f1324737cb2815e47b56f8f76
Ruffa <ruffaresentes@me.com>
2025-03-07 15:46:10 +0800

    weather get and send text/whatsapp

--- day35_rain_alert/main.py
+++ day35_rain_alert/main.py
@@ -1,6 +1,23 @@
 import requests
+from twilio.rest import Client
+
 OWM_Endpoint="https://api.openweathermap.org/data/2.5/forecast"
-api_key = "825a9682f806aef7999ce7324fc996a0"
+api_key = ""
+
+#twilio account side
+account_sid = ""
+auth_token = ""
+
+def send_sms():
+    client = Client(account_sid, auth_token)
+    message = client.messages.create(
+        body="Bring an umbrella. ☂️ Uulan mamaya!",
+        from_="whatsapp:+14155238886",
+        to="whatsapp:YOUR NUMBER",
+    )
+
+    print(message.status)
+
 
 weather_params = {
     "lat": 11.694760,
@@ -10,7 +27,14 @@ weather_params = {
 }
 
 response = requests.get(OWM_Endpoint, params=weather_params)
-
+will_rain = False
 weather_data = response.json()
 for hour_data in weather_data["list"]:
-    print(hour_data)
+    condition_code = hour_data["weather"][0]["id"]
+    if int(condition_code) < 700:
+        will_rain = True
+if will_rain:
+    send_sms()
+    print("done sent a text 'Bring an umbrella. ☂️'")
+
+
